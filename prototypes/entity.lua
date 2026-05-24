1b5f57f389d3cfe907ea31b3e63b46dfad5a6b33
TetrisBattle <thien5596@gmail.com>
2026-05-25 02:24:55 +0300

    feat: make mythos building 4x4

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -7,14 +7,14 @@ data:extend({
     flags = {"placeable-neutral", "player-creation", "not-rotatable"},
     minable = {mining_time = 0.5, result = "mythos"},
     max_health = 100,
-    collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
-    selection_box = {{-1, -1}, {1, 1}},
+    collision_box = {{-1.7, -1.7}, {1.7, 1.7}},
+    selection_box = {{-2, -2}, {2, 2}},
     picture = {
       filename = "__mythos__/graphics/entities/mythos.png",
       priority = "high",
       width = 64,
       height = 64,
-      scale = 1.0,
+      scale = 2.0,
     },
     inventory_size = 1,
   }
